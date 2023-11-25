98770aedc9f54eade629d223d3bc223d984a337a
Anastasia Chernova <anastasia.altera@gmail.com>
2023-11-25 20:24:35 +0700

    def print

---
+++ lesson_1_task_7.py
@@ -0,0 +1,40 @@
+def print_1():
+    print(1)
+
+def print_2():
+    print(2)
+
+def print_3():
+    print(3)
+
+def print_4():
+    print(4)
+
+def print_5():
+    print(5)
+
+def print_6():
+    print(6)
+
+def print_7():
+    print(7)
+
+def print_8():
+    print(8)
+
+def print_9():
+    print(9)
+
+def print_0():
+    print(0)
+
+print_8()
+print_8()
+print_0()
+print_0()
+print_5()
+print_5()
+print_5()
+print_3()
+print_5()
+print_3()
