f36ce780f2e6a20e3b5fbbf4d54f3869e35f8335
Anastasia Chernova <anastasia.altera@gmail.com>
2023-11-25 20:27:55 +0700

    print_number

---
+++ lesson_1_task_8.py
@@ -0,0 +1,14 @@
+def print_number(num):
+    print(num)
+
+print_number(8)
+print_number(8)
+print_number(0)
+print_number(0)
+print_number(5)
+print_number(5)
+print_number(5)
+print_number(3)
+print_number(5)
+print_number(3)
+print_number(5)
