b665b6eb3221eac830d0936af3a266d83c3f3d08
Anastasia Chernova <anastasia.altera@gmail.com>
2023-11-27 20:10:15 +0700

    «Вас зовут: last_name first_name»

--- lesson_1_task_4.py
+++ lesson_1_task_4.py
@@ -1,3 +1,3 @@
-first_name = "Валикадия"
-last_name = "Белуха"
-input ("Вас зовут: " + first_name + " " + last_name)
+first_name = input ("Ваше имя: ")
+last_name = input ("Ваша фамилия: ")
+print ("Вас зовут: " + first_name , last_name)
