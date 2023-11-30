f02da7741d5994c5554f56cb35a43e2638c1f0b0
Anastasia Chernova <anastasia.altera@gmail.com>
2023-11-30 17:30:34 +0700

    88005553535

--- lesson_1_task_8.py
+++ lesson_1_task_8.py
@@ -1,43 +1,14 @@
-def print_1():
-    num_1 = input ("Введитe число 1: ")
-    print (num_1, end='')
-def print_2():
-    num_2 = input ("Введитe число 2: ")
-    print (num_2, end='')
-def print_3():
-    num_3 = input ("Введитe число 3: ")
-    print (num_3, end='')
-def print_4():
-    num_4 = input ("Введитe число 4: ")
-    print (num_4, end='')
-def print_5():
-    num_5 = input ("Введитe число 5: ")
-    print (num_5, end='')
-def print_4():
-    num_6 = input ("Введитe число 6: ")
-    print (num_6, end='')
-def print_7():
-    num_7 = input ("Введитe число 7: ")
-    print (num_7, end='')
-def print_8():
-    num_8 = input ("Введитe число 8: ")
-    print (num_8, end='')
-def print_9():
-    num_9 = input ("Введитe число 9: ")
-    print (num_9, end='')
-def print_0():
-    num_0 = input ("Введитe число 0: ")
-    print (num_0, end='')
+def println(num):
+    print (num, end='')
 
-
-print_8 ()
-print_8 ()
-print_0 ()
-print_0 ()
-print_5 ()
-print_5 ()
-print_5 ()
-print_3 ()
-print_5 ()
-print_3 ()
-print_5 ()
+println (8)
+println (8)
+println (0)
+println (0)
+println (5)
+println (5)
+println (5)
+println (3)
+println (5)
+println (3)
+println (5)
