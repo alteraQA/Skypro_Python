edde2bc088e3ea2fddebd166c265d730b3017e4b
Anastasia Chernova <anastasia.altera@gmail.com>
2023-12-17 19:17:56 +0700

    for in range

--- Lesson_2/lesson_2_for.py
+++ Lesson_2/lesson_2_for.py
@@ -1,4 +1,7 @@
 for  x in range(1, 21):
     print("x= ", x, "x2= ", x*x)
 
-    
+students=["Вася", "Гоша", "Ира", "Валя", "Женя", "Мефодий"]
+l=len(students)
+for i in range(0, l):
+    print(students[i])
