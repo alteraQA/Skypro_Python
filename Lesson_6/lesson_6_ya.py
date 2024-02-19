865fa3592c410b770a3f28435f8f47dd175ae421
Anastasia Chernova <anastasia.altera@gmail.com>
2024-02-19 12:05:10 +0700

    OperaDriverManager

--- Lesson_6/lesson_6_ya.py
+++ Lesson_6/lesson_6_ya.py
@@ -6,11 +6,15 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 
-driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+from selenium import webdriver
+from selenium.webdriver.chrome import service
+from webdriver_manager.opera import OperaDriverManager
+
+##driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 
 ##driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())) 
 
 driver.maximize_window()
 driver.get("http://ya.ru/")
 driver.save_screenshot('./test.png')
-driver.quit
+driver.quit()
