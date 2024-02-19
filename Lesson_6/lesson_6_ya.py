93d7bd87f2620fcb9742c207dfb956223e4e9e02
Anastasia Chernova <anastasia.altera@gmail.com>
2024-02-19 12:08:15 +0700

    EdgeService

--- Lesson_6/lesson_6_ya.py
+++ Lesson_6/lesson_6_ya.py
@@ -1,15 +1,18 @@
 from time import sleep 
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 
 from selenium import webdriver
+from selenium.webdriver.edge.service import Service as EdgeService
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
-driver = webdriver.Edge(EdgeChromiumDriverManager().install())
+driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
 
 ##driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 
