d46a62d76326810080cce7edeaf9420054010106
Anastasia Chernova <anastasia.altera@gmail.com>
2024-02-19 12:01:50 +0700

    GeckoDriverManager

--- Lesson_6/lesson_6_ya.py
+++ Lesson_6/lesson_6_ya.py
@@ -2,10 +2,15 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())) 
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
+##driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())) 
+
 driver.maximize_window()
 driver.get("http://ya.ru/")
-sleep(5)
-driver.save_screenshot('./test.jpg')
+driver.save_screenshot('./test.png')
 driver.quit
