9b896905e4394d43ce4dfa76144839ce998f3740
Anastasia Chernova <anastasia.altera@gmail.com>
2024-02-19 11:51:48 +0700

    lesson_6

---
+++ Lesson_6/lesson_6_ya.py
@@ -0,0 +1,10 @@
+from time import sleep 
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())) 
+driver.maximize_window()
+driver.get("http://ya.ru/")
+sleep(5)
+driver.save_screenshot('./test.jpg')
